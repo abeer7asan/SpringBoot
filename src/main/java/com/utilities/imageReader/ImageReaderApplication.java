153bb7e4997d35a458b215e73026d6272494fc79
abeer7asan <abeer7asan3@gmail.com>
2021-08-26 12:59:21 +0300

    Initial Upload

---
+++ src/main/java/com/utilities/imageReader/ImageReaderApplication.java
@@ -0,0 +1,13 @@
+package com.utilities.imageReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageReaderApplication.class, args);
+	}
+
+}
