f5436e8fc6272879a45bc458934ae044d70fda0b
abeer7asan <abeer7asan3@gmail.com>
2021-08-26 13:12:12 +0300

    Add files via upload

---
+++ imageReader/src/test/java/com/utilities/imageReader/ImageReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utilities.imageReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
